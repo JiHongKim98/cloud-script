c4b2fcc8b0eabded3a9afd38cb45e5e678514e9f
JiHongKim98 <kinjihong9598@gmail.com>
2024-03-26 18:52:35 +0900

    feat: Add aws ec2 swap memory script

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # aws-script
 AWS에서 사용할 스크립트 모음
+
+ - [EC2 Swap Memory 설정(2GB)](/swap-memory.sh)
---
+++ swap-memory.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# 128MB x 16 = 2GB swap 메모리
+sudo dd if=/dev/zero of=/swapfile bs=128M count=16
+# 읽기 및 쓰기 권한 부여
+sudo chmod 600 /swapfile
+
+# Linux 스왑 영역 설정
+sudo mkswap /swapfile
+
+# swap 메모리 활성화
+sudo swapon /swapfile
+sudo swapon -s
+
+# 재부팅시에도 스왑 파일 적용
+echo '/swapfile swap swap defaults 0 0' >> /etc/fstab
